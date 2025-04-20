cf1d3b351924ae0fbbe8f383357b192e3793a96a
Jayant Modhave <propubggamers7@gmail.com>
2025-04-21 01:00:50 +0530

    Deleted Previous files

---
+++ 02_Singly_Linked_List/02_DisplaySinglyLinkedList.c
@@ -0,0 +1,117 @@
+/* 
+    write a c program to Create and Display Singly Linked list. with structure "Student" with following data members:
+	-Name,
+	-Roll no,
+	-Standard,
+	-Division.
+*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct SinglyLinkedList{
+	
+	int rollNo;
+	char name[20];
+	
+	struct SinglyLinkedList * next;	
+}SLL;
+
+SLL * CreateLinkedList(SLL * Head){
+	char ans;
+	 SLL * Node = NULL;
+	 SLL * last = Head;
+    do{
+		
+	    Node = (SLL *)malloc(sizeof(SLL));
+		Node->next = NULL;
+	
+	    printf("\n\t**********Enter Students details**********\n");
+		printf("Enter the Name of Student: ");
+		scanf(" %[^\n]s",Node->name);
+		
+		printf("Enter the RollNo: ");
+		scanf(" %d",&Node->rollNo);
+	
+	
+		if(Head == NULL)
+		{
+			Head = Node; 
+		}
+		else{
+			last->next = Node;		
+		}
+		last = Node;
+				
+			printf("\nDo you want to enter new node? [Y/N]: ");
+			scanf(" %c",&ans);				
+	}while((ans=='y')||(ans=='Y'));	
+	
+
+	last = NULL;
+	free(last);
+	
+	Node = NULL;
+	free(Node);
+		
+	return Head;
+
+}
+
+
+void DisplaySinglyLinkedList(SLL * Head)
+{
+	if(Head == NULL)
+	{
+		printf("\n!!!The List is Empty!!!\n");
+	}
+	else
+	{		
+		for(;Head!=NULL;Head=Head->next)
+		{
+			printf(" Roll no : %d \n Name : %s  \n------------------------------------------------------------------------------------------------------------------------\n",Head->rollNo,Head->name);
+		}	   
+	}
+}
+
+int main()
+{
+    //code
+	char ch; 
+	int Choice;
+	SLL * Start = NULL;
+	
+	
+	do{
+		printf("\n********** Singly Linked List **********\n");
+		printf("\nEnter 1: to Create Singly Linked list\n");
+    	printf("Enter 2: to Display Singly Linked List\n");	
+		printf("Enter 3: to Exit the Linked List\n");
+	
+			printf("\nChoice: ");			
+			scanf("%d",&Choice);
+
+		switch (Choice){
+			
+			case 1:	
+					Start = NULL;
+					Start = CreateLinkedList(Start);
+			break;
+		
+			case 2: 
+					printf("\n\t***Students Data***\n");
+					DisplaySinglyLinkedList(Start);
+			break;
+			
+			case 3:
+			        printf("\n\n Exiting the linked list ....");
+					free(Start);
+					Start = NULL;
+					exit(0);
+			break;
+			
+		}			
+	}while(Choice!=3);
+		
+	return 0;
+}
